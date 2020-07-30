f8801c18f7c3608728f00d62b10bb1cc494a2f2f
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-07-31 01:44:20 +0530

    Create 8_list_comprehension.py

---
+++ 8_list_comprehension.py
@@ -0,0 +1,10 @@
+#You are given three integers  and  representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise.
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a + b + c != n ])    
