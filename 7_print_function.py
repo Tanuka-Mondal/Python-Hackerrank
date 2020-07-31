84285f5c84e4530e088ca156e0d9720e29cf50a7
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-01 01:00:51 +0530

    Create 7_print_function.py

---
+++ 7_print_function.py
@@ -0,0 +1,17 @@
+/* The included code stub will read an integer, , from STDIN.
+
+Without using any string methods, try to print the following:
+
+
+Note that "" represents the consecutive values in between. */
+
+#---------------------------------------------------------------
+
+if __name__ == '__main__':
+    n = int(input())
+
+lst=[]
+for i in range(1,n+1):
+    lst.append(i)
+    
+print(''.join(str(a) for a in lst))
