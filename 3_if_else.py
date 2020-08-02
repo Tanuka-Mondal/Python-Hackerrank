5943968d8d76b0e8425f6f2aaf24c40c51552024
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 22:38:03 +0530

    Update 3_if_else.py

--- 3_if_else.py
+++ 3_if_else.py
@@ -1,9 +1,11 @@
-/* Given an integer, , perform the following conditional actions:
+"""
+Given an integer, , perform the following conditional actions:
 
 If  is odd, print Weird
 If  is even and in the inclusive range of  to , print Not Weird
 If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird */
+If  is even and greater than , print Not Weird 
+"""
 
 #-----------------------------------------------------------------
 
