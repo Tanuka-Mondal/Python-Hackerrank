02035e57f8c2d33678f3d899f32cffc54170ee95
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 22:37:31 +0530

    Update 4_division.py

--- 4_division.py
+++ 4_division.py
@@ -1,6 +1,8 @@
-/* The provided code stub reads two integers,  and , from STDIN.
+"""
+The provided code stub reads two integers,  and , from STDIN.
 
-Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / . */
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / . 
+"""
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
