956b2717f59329e43c5530dcea4be5ed51b1cea8
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 22:38:36 +0530

    Update 2_arithmetic_operators.py

--- 2_arithmetic_operators.py
+++ 2_arithmetic_operators.py
@@ -1,8 +1,10 @@
-/* The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+"""
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
 
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers. */
+The third line contains the product of the two numbers.
+"""
 
 #----------------------------------------------------------------------------------------------------
 
