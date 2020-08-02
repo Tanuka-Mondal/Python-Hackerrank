30b25be259a88a4158ce45f361043b1b9b737578
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-08-02 22:36:41 +0530

    Update 7_print_function.py

--- 7_print_function.py
+++ 7_print_function.py
@@ -1,9 +1,11 @@
-/* The included code stub will read an integer, , from STDIN.
+"""
+The included code stub will read an integer, , from STDIN.
 
 Without using any string methods, try to print the following:
 
+Note that "" represents the consecutive values in between. 
+"""
 
-Note that "" represents the consecutive values in between. */
 
 #---------------------------------------------------------------
 
